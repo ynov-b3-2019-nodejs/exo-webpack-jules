5738b5541dce5fea3f9baf5f6dfd1c5a5ef33049
Mathieu TUDISCO <oss@mathieutu.dev>
2019-06-24 18:21:44 +0200

    Add components.

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+
+import reactLogo from './assets/React-icon.png';
+import { Home } from './Home';
+import { About } from './About';
+
+export const App = () => (
+  <BrowserRouter>
+    <main className="container">
+      <div>
+        <h1>Coucou Jules !</h1>
+        <img className="container__image" alt="react logo" src={reactLogo}/>
+        <p>Si tu vois ça, c'est que ça marche !</p>
+      </div>
+      <ul className="left">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </Switch>
+    </main>
+  </BrowserRouter>
+);
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+
+import './style.scss';
+
+const root = document.getElementById('app');
+
+ReactDOM.render(React.createElement(App), root);
+
